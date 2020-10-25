0d8e9d5562a86ceaaa4323ad9ee5eb801ac1fb33
Max <maxim.macari.dam@gmail.com>
2020-10-25 22:17:15 +0100

    Initial Commit

---
+++ Side Tab Bar/Side_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Side_Tab_BarApp.swift
+//  Side Tab Bar
+//
+//  Created by Maxim Macari on 25/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Side_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
